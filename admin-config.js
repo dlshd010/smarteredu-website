ddce12640c72b66fa2b44b5383b3ff35924ea987
Dilshad Hussain <dilshad.rock256@gmail.com>
2026-08-15 12:17:04 +0530

    Fix string formatting for Supabase constants

--- admin-config.js
+++ admin-config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL = https://hcxszcypmauarmwqkxjj.supabase.co/rest/v1/;
-const SUPABASE_PUBLISHABLE_KEY = sb_publishable_zB0ejL0_OUqitUx_UgX9bQ_m-ZLx258;
+const SUPABASE_URL = "https://hcxszcypmauarmwqkxjj.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_zB0ejL0_OUqitUx_UgX9bQ_m-ZLx258";
